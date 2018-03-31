7e6b2e59268f8e8b10a2cc54f569718dd69b9fde
BrightSkyz <BrightSkyz@users.noreply.github.com>
2018-03-31 11:08:50 -0400

    Made it so ">support parameters" wouldn't break the bot as it was detected as a parameter.

--- src/main/java/com/namelessmc/discord/cmds/supportCmd.java
+++ src/main/java/com/namelessmc/discord/cmds/supportCmd.java
@@ -31,22 +31,27 @@ public class supportCmd extends ListenerAdapter {
                 } else {
                     String[] args = event.getMessage().getContentRaw().replace(Bot.BOT_PREFIX + "support ", "").split(" ");
                     JSONObject supportJson = null;
-                    try {
-                        supportJson = fetchJson.fromUrl("https://raw.githubusercontent.com/NamelessMC/BotConfiguration/" + commitHash + "/support/" + URLEncoder.encode(args[0], "UTF-8") + ".json");
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                    if (supportJson.getInt("responseCode") == 404) {
+                    if (args[0].equalsIgnoreCase("parameters")) {
                         embedBuilder = new EmbedBuilder().setTitle("Support").setColor(Bot.EMBED_COLOR)
-                            .setDescription(getParameterMessage(commitHash));
+                                .setDescription(getParameterMessage(commitHash));
                     } else {
-                        String description = "";
-                        for (Object line : supportJson.getJSONArray("body")) {
-                            String stringLine = (String) line;
-                            description = description + stringLine.replace("\\n", System.lineSeparator());
+                        try {
+                            supportJson = fetchJson.fromUrl("https://raw.githubusercontent.com/NamelessMC/BotConfiguration/" + commitHash + "/support/" + URLEncoder.encode(args[0], "UTF-8") + ".json");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
                         }
-                        embedBuilder = new EmbedBuilder().setTitle("Support: " + supportJson.getString("title")).setColor(Bot.EMBED_COLOR)
-                            .setDescription(description);
+                        if (supportJson.getInt("responseCode") == 404) {
+                            embedBuilder = new EmbedBuilder().setTitle("Support").setColor(Bot.EMBED_COLOR)
+                                    .setDescription(getParameterMessage(commitHash));
+                        } else {
+                            String description = "";
+                            for (Object line : supportJson.getJSONArray("body")) {
+                                String stringLine = (String) line;
+                                description = description + stringLine.replace("\\n", System.lineSeparator());
+                            }
+                            embedBuilder = new EmbedBuilder().setTitle("Support: " + supportJson.getString("title")).setColor(Bot.EMBED_COLOR)
+                                    .setDescription(description);
+                        }
                     }
                 }
                 event.getChannel().sendMessage(embedBuilder.build()).queue();
