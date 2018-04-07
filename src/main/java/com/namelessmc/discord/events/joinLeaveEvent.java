be3c49d21789e0e15f99a8b7843d558ee238bbae
BrightSkyz <BrightSkyz@users.noreply.github.com>
2018-04-07 12:02:37 -0400

    Add countdown

--- src/main/java/com/namelessmc/discord/events/joinLeaveEvent.java
+++ src/main/java/com/namelessmc/discord/events/joinLeaveEvent.java
@@ -22,12 +22,30 @@ public class joinLeaveEvent extends ListenerAdapter {
                     memberCount++;
                 }
             }
+            String footerText = "There are now " + memberCount + " members.";
+            Integer membersToGo = 1000 - memberCount;
+            if (membersToGo > 0) {
+                if (membersToGo == 1) {
+                    footerText = footerText + " (" + membersToGo + " more member until 1,000!)";
+                } else {
+                    footerText = footerText + " (" + membersToGo + " more members until 1,000.)";
+                }
+            } else if (membersToGo > 0) {
+                membersToGo = membersToGo * -1;
+                if (membersToGo == 1) {
+                    footerText = footerText + " (We now have " + membersToGo + " more member than 1,000!)";
+                } else {
+                    footerText = footerText + " (We now have " + membersToGo + " more members than 1,000!)";
+                }
+            } else {
+                footerText = footerText + " (We've hit 1000 members! :tada: )";
+            }
             if (event.getGuild().getTextChannelsByName(Bot.WELCOME_MESSAGE_CHANNEL, true).size() > 0) {
                 TextChannel welcomeChannel = event.getGuild().getTextChannelsByName(Bot.WELCOME_MESSAGE_CHANNEL, true).get(0);
                 if (canTalkCustom.canTalk(welcomeChannel, true)) {
                     EmbedBuilder embedBuilder = new EmbedBuilder().setTitle("User Join").setColor(Bot.EMBED_COLOR_GREEN)
                             .setDescription("Welcome ``" + cleanUsername + "#" + eventUser.getDiscriminator() + "`` to the NamelessMC Discord.")
-                            .setFooter("There are now " + memberCount + " members.", null);
+                            .setFooter(footerText, null);
                     welcomeChannel.sendMessage(embedBuilder.build()).queue();
                 }
             }
@@ -45,12 +63,30 @@ public class joinLeaveEvent extends ListenerAdapter {
                     memberCount++;
                 }
             }
+            String footerText = "There are now " + memberCount + " members.";
+            Integer membersToGo = 1000 - memberCount;
+            if (membersToGo > 0) {
+                if (membersToGo == 1) {
+                    footerText = footerText + " (" + membersToGo + " more member until 1,000!)";
+                } else {
+                    footerText = footerText + " (" + membersToGo + " more members until 1,000.)";
+                }
+            } else if (membersToGo > 0) {
+                membersToGo = membersToGo * -1;
+                if (membersToGo == 1) {
+                    footerText = footerText + " (We now have " + membersToGo + " more member than 1,000!)";
+                } else {
+                    footerText = footerText + " (We now have " + membersToGo + " more members than 1,000!)";
+                }
+            } else {
+                footerText = footerText + " (We've hit 1000 members! :tada: )";
+            }
             if (event.getGuild().getTextChannelsByName(Bot.WELCOME_MESSAGE_CHANNEL, true).size() > 0) {
                 TextChannel welcomeChannel = event.getGuild().getTextChannelsByName(Bot.WELCOME_MESSAGE_CHANNEL, true).get(0);
                 if (canTalkCustom.canTalk(welcomeChannel, true)) {
                     EmbedBuilder embedBuilder = new EmbedBuilder().setTitle("User Leave").setColor(Bot.EMBED_COLOR_RED)
                             .setDescription("Goodbye ``" + cleanUsername + "#" + eventUser.getDiscriminator() + "``. Thanks for visiting the NamelessMC Discord.")
-                            .setFooter("There are now " + memberCount + " members.", null);
+                            .setFooter(footerText, null);
                     welcomeChannel.sendMessage(embedBuilder.build()).queue();
                 }
             }
