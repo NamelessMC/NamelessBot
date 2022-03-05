4c355cb5fa258b58cf4d6f0b7ac39155ef279cc6
Supercrafter100 <stijn.voeten@telenet.be>
2022-03-05 20:35:14 +0100

    add no-ping functionality

---
+++ src/events/no-ping.event.ts
@@ -0,0 +1,40 @@
+import { Message, MessageEmbed, TextChannel } from "discord.js";
+import { Event } from "../handlers/EventHandler";
+import { client } from "..";
+
+export default class InteractionCreate extends Event<"messageCreate"> {
+    public event = "messageCreate";
+
+    public async run(msg: Message) {
+        if (
+            !msg.guild
+            || msg.guild?.id !== this.client.config.guildID
+            || msg.mentions.members?.size === 0
+            || msg.reference !== null
+            || msg.member?.user.bot
+        )
+            return;
+
+        const senderIsStaff = msg.member?.roles.cache.some((role) =>
+            this.client.config.supportRoles.includes(role.id)
+        );
+
+        if (senderIsStaff) {
+            return;
+        }
+
+        const mentionsStaff = msg.mentions.members?.some((member) =>
+            member.roles.cache.some((role) =>
+                this.client.config.supportRoles.includes(role.id)
+            )
+        );
+
+        if (mentionsStaff) {
+            await msg.channel.send(
+                `Hey ${
+                    msg.member?.nickname ?? msg.author.username
+                }! Please don't tag helpful/staff members directly.`
+            );
+        }
+    }
+}
